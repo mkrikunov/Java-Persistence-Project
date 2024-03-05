42e2a6cbd98b6794719d0954aaf207ea9de95b02
Olga <o.pogibelnaya@g.nsu.ru>
2024-03-06 00:13:41 +0700

    Черновик по сериализации класса в json файл

--- lib/src/main/java/persistence/project/Main.java
+++ lib/src/main/java/persistence/project/Main.java
@@ -1,40 +1,66 @@
 package persistence.project;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-  private final OutputStream outputStream;
+  private final String filePath;
 
-  public Main(String filePath) throws FileNotFoundException {
-    outputStream = new FileOutputStream(filePath);
+  public Main(String filePath) {
+    this.filePath = filePath;
   }
 
   public void serialize(Object object) {
     Field[] fields = object.getClass().getDeclaredFields();
+    int n = fields.length;
+    List<String> names = new ArrayList<>(n);
+    List<Object> values = new ArrayList<>(n);
+    List<Class<?>> types = new ArrayList<>(n);
+    List<String> modifiers = new ArrayList<>(n);
 
     for (Field field : fields) {
       field.setAccessible(true);
-      Object value;
+      names.add(field.getName());
+
       try {
-        value = field.get(object);
+        values.add(field.get(object));
       } catch (IllegalAccessException e) {
         throw new RuntimeException(e);
       }
-      Class<?> type = field.getType();
+      types.add(field.getType());
 
-      int modifiers = field.getModifiers();
-      String access = java.lang.reflect.Modifier.toString(modifiers);
-
-      //outputStream.write();
-      System.out.println("Value: " + value);
-      System.out.println("Type: " + type);
-      System.out.println("Access: " + access);
+      int modifiersField = field.getModifiers();
+      String access = java.lang.reflect.Modifier.toString(modifiersField);
+      modifiers.add(access);
     }
+
+    writeToFile(values, types, modifiers, names, n);
   }
 
+  private void writeToFile(List<Object> values, List<Class<?>> types, List<String> modifiers,
+      List<String> names, int n) {
+    try (OutputStream outputStream = new FileOutputStream(filePath, true)) {
+      outputStream.write("{\n".getBytes());
+      outputStream.write("  \"class\": {\n".getBytes());
+      outputStream.write("    \"id\": 1,\n".getBytes());
+      outputStream.write("    \"name\": \"ExampleClass\",\n".getBytes());
+      outputStream.write("    \"fields\": [\n      {\n".getBytes());
 
+      for (int i = 0; i < n; i++) {
+        outputStream.write(("        \"name\": \"" + names.get(i) + "\",\n").getBytes());
+        outputStream.write(("        \"type\": \"" + types.get(i) + "\",\n").getBytes());
+        outputStream.write(("        \"value\": \"" + values.get(i) + "\",\n").getBytes());
+        outputStream.write(("        \"access\": \"" + modifiers.get(i) + "\"\n      }\n").getBytes());
+      }
+
+      outputStream.write("    ]\n  }\n}".getBytes());
+    } catch (IOException e) {
+      System.out.println("Ошибка при добавлении данных в файл: " + e.getMessage());
+    }
+  }
 }
--- lib/src/test/java/persistence/project/LibraryTest.java
+++ lib/src/test/java/persistence/project/LibraryTest.java
@@ -1,18 +1,14 @@
 package persistence.project;
 
-import java.io.FileNotFoundException;
 import org.junit.jupiter.api.Test;
 import persistence.project.examples.Animal;
 
 class LibraryTest {
-    @Test void someLibraryMethodReturnsTrue() {
-        Animal obj = new Animal("Cat");
-      Main main;
-      try {
-        main = new Main("src/main/resources/data.json");
-      } catch (FileNotFoundException e) {
-        throw new RuntimeException(e);
-      }
-      main.serialize(obj);
-    }
+
+  @Test
+  void someLibraryMethodReturnsTrue() {
+    Animal obj = new Animal("Cat");
+    Main main = new Main("src/main/resources/data.json");
+    main.serialize(obj);
+  }
 }
