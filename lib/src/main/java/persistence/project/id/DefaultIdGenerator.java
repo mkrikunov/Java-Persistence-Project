d60058fb50cf746a2590fbd5dc07eb8c05936801
Mikhail <misha_k_2018@mail.ru>
2024-03-05 23:55:44 +0700

    Реализовал IdGenerator и написал тест

--- lib/src/main/java/persistence/project/id/DefaultIdGenerator.java
+++ lib/src/main/java/persistence/project/id/DefaultIdGenerator.java
@@ -1,10 +1,39 @@
 package persistence.project.id;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class DefaultIdGenerator implements IdGenerator{
+  private final String fileName = "src/main/resources/currentID.txt";
+
+  private String getCurrId() {
+    String id = "";
+
+    try (FileReader fileReader = new FileReader(fileName);
+        BufferedReader reader = new BufferedReader(fileReader)) {
+      id = reader.readLine().trim();
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+      System.exit(1);
+    }
+    return id;
+  }
+
+  private void setCurrId(int id) {
+    try (FileWriter fileWriter = new FileWriter(fileName)) {
+      fileWriter.write(Integer.toString(id));
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+      System.exit(1);
+    }
+  }
 
   @Override
   public String generateId() {
-    //берем id из файла в ресурсах, увеличиваем на один и возвращаем + записываем в файл
-    return "1";
+    String id = getCurrId();
+    setCurrId(Integer.parseInt(id) + 1);
+    return id;
   }
 }
